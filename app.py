98ae175dc85604e5c42fa3b3a290e20ed4fc6e86
c-ravikiran <131195983+c-ravikiran@users.noreply.github.com>
2023-04-19 00:36:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,6 @@ second = st.number_input("Second number", value = 0)
 third = st.number_input("Third number", value = 0)
 
 #Button
-Max = st.radio(
-    "Max value of numbers",
-    ('M'))
-
-if Max == 'M':
+Max = st.button("Max value of numbers"):
     largest_num = max_value(first, second, third)
     st.write(f"The largest number is {largest_num}")
